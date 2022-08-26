305b1587b5798c219de6d20d56428e0ba90d24f9
benjarat031 <buabenjarat3009@gmail.com>
2022-08-26 09:50:10 +0700

    first commit

---
+++ from/header.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ben Jax</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@300&display=swap" rel="stylesheet">
+<style>
+    body{
+        font-family: 'Prompt', sans-serif;
+    }
+</style>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+</head>
+  <body>
+    
+  <nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand text-light" href="#"><img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/287362875_754440509222288_6416856110550362868_n.png?stp=dst-png_p206x206&_nc_cat=107&ccb=1-7&_nc_sid=aee45a&_nc_eui2=AeHtSNmPCW2HYUEnnbjUsP9ZwO3ZgTU7VdTA7dmBNTtV1ActzAY6d7piBFSabdfdZCrvbF2tfMG0aMr3Xv62N0pe&_nc_ohc=wH3fF6nklPQAX-8ztte&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVJV_xA_A4N1c2fmwcXPcnW13ZdTeTB2BdQaJAnO3mPlhA&oe=6312C448" alt="" width="30" height="24" class="d-inline-block align-text-top"> Ben Jax</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active text-light active" aria-current="page" href="index.php"><i class="bi bi-house-door"></i> หน้าแรก</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-light active" aria-current="page" href="frmRegister.php"><i class="bi bi-person"></i> สมัครสมาชิก</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <button class="btn btn-outline-light text-light" type="submit"><i class="bi bi-box-arrow-in-right"></i> Login</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+  </body>
+</html>
---
+++ from/insert.php
@@ -0,0 +1,24 @@
+<?php
+    $firstname = $_POST['txt_firstname'];
+    $name = $_POST['txt_name'];
+    $lastname = $_POST['txt_lastname'];
+    $sex = $_POST['txt_sex'];
+    $birthdate = $_POST['txt_birthdate'];
+    $address = $_POST['txt_address'];
+    $tle = $_POST['txt_tle'];
+    $email = $_POST['txt_email'];
+
+    echo $firstname;
+    echo $name;
+    echo $lastname;
+    echo "<br>";
+    echo $sex;
+    echo "<br>";
+    echo $birthdate;
+    echo "<br>";
+    echo $address;
+    echo "<br>";
+    echo $tle;
+    echo "<br>";
+    echo $email;
+?>
